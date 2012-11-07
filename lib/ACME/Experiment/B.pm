16455d747f5a282d2490bfc7fd0c0b29c51a5857
Neil Bowers <neil@bowers.com>
2012-11-07 11:08:27 +0000

    Import of NEILB/ACME-Experiment-C-0.01 from CPAN.
    gitpan-cpan-distribution: ACME-Experiment-C
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/ACME-Experiment-C-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/ACME/Experiment/B.pm
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+package ACME::Experiment::B;
+
+1;
---
+++ lib/ACME/Experiment/C.pm
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+package ACME::Experiment::C;
+
+=head1 NAME
+
+ACME::Experiment::C - experiments with PAUSE permissions
+
+=head1 DESCRIPTION
+
+I'm experimenting with various things while talking to ANDK.
+
+=cut
+
+1;
